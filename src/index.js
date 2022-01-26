74ff7bf51480eaefc79aa7aaaa2bdf5b89e3ed1e
OlgaGre <grechanyuko@gmail.com>
2022-01-26 11:45:39 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let num = String(n).split('').reverse().join('')
+    if (n >= 0) {
+        return num
+    } else {
+        return num.slice(0, num.length - 1)
+    }
 }
